ed291f93f70d2f25101128caac51f74eface1352
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-06-09 09:08:22 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,11 +1,11 @@
 var trace1 = {
-    x: [1, 2, 3, 4, 5],
+    x: [2000, 2005, 2010, 2015, 2020],
     y: [1, 3, 9, 27, 81],
     type: 'scatter',
     name: 'Regular Computer'
 };
 var trace2 = {
-    x: [1, 2, 3, 4, 5],
+    x: [2000, 2005, 2010, 2015, 2020],
     y: [1, 4, 16, 64, 256],
     type: 'scatter',
     name: 'Quantum Computer'
